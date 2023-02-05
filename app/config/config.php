c8091289a8dd66ff03d66437a7bf90d6ad8bc215
mulzahrian <mulzahrianone@gmail.com>
2023-02-05 09:17:15 +0700

    IDIM 5 TEAM V.01

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/idimMVC/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tk3_team_5');
---
+++ app/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">TEAM 5 WEB</h1>
+      
+    </div>
+
+
+</div>
